45f77b6413b6f8ac2ab70477986649b80c193dfe
Twiknight <twiknight@163.com>
2015-11-29 14:06:27 +0800

    Revert "Revert "v1.0""
    This reverts commit e63b8d8504d4e33573f9b09337a9e4f2b9328a03.
    
    Conflicts:
    	site.json

---
+++ src/components/Banner.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="banner">
+        <h1>{{$parent.site.title}}</h1>
+        <h1><small>{{$parent.site.subtitle}}</small></h1>
+    </div>
+</template>
+
+<style scoped lang="less">
+@backgroundColor :  #333;
+@fontColor: #fff;
+@darkenAmount: 20%;
+
+.banner {
+    background: @backgroundColor;
+    padding: 100px 0 ;
+    h1 {
+        text-align: center;
+        color: @fontColor;
+        small {
+            color: darken(@fontColor, @darkenAmount);
+        }
+    }
+}
+</style>
+
+<script lang="babel">
+    export default {
+        name : 'Banner'
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import Vue from 'vue';
-import Router from 'vue-router';
+// import Router from 'vue-router';
 import App from './components/app.vue';
-import BlogHeader from './components/blogHeader.vue';
+import Banner from './components/Banner.vue';
+import BlogHeader from './components/BlogHeader.vue';
 
-Vue.use(Router);
+// Vue.use(Router);
 
-let router = new Router();
+// let router = new Router();
 
-Vue.component("blogheader", BlogHeader);
-router.start(App,"#main");
+new Vue({
+    el:"body",
+    components: {
+        app:App
+    }
+});
